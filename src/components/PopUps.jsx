7a99e61bdbb47c4c2a02a3dac859cacd0500b294
Adarshjain3011 <adarshjain3011@gamil.com>
2025-01-24 12:10:11 +0530

    minor changes

--- src/components/PopUps.jsx
+++ src/components/PopUps.jsx
@@ -14,14 +14,18 @@ import { useDispatch } from 'react-redux';
 
 import { isValidEmail } from '../utils';
 
-function PopUps({ setshowPopUps, showPopUps,emailSearch ,setSearchedResponseData}) {
+
+function PopUps({ setshowPopUps, showPopUps, emailSearch, setSearchedResponseData,setError }) {
 
   const dispatch = useDispatch();
+
+  const data = useSelector((state) => state.user.data);
 
   const [loading, setLoading] = useState(false);
 
   async function checkStatusHandler() {
 
+    console.log("aa gaye yaar ");
 
     try {
 
@@ -32,7 +36,7 @@ function PopUps({ setshowPopUps, showPopUps,emailSearch ,setSearchedResponseData
 
         toast.error("plz provide the email");
         setshowPopUps(false);
-
+        console.log("1")
         return;
       }
 
@@ -42,35 +46,50 @@ function PopUps({ setshowPopUps, showPopUps,emailSearch ,setSearchedResponseData
 
         setshowPopUps(false);
 
+        console.log("2")
+
         return;
 
       }
 
+      console.log("5");
 
-      setshowPopUps(false);
+      // if (Number(data?.credits) === 0) {
 
-      if (Number(data?.credits) === 0) {
+      //   console.log("4");
 
-        toast.error("you do not have enough  credits left");
-        return
-      }
+      //   toast.error("you do not have enough  credits left");
+      //   return
+      // }
 
-      const response = await axios.post(`${backendUrl}/api/users/user-info`, {
+      console.log("hellow");
+
+      let backendUrl = "http://localhost:4000/api/users/user-info";
+
+      console.log("bac", backendUrl);
+
+      const response = await axios.post(backendUrl, {
 
         email: emailSearch,
         userId: data._id
 
       });
 
-      console.log("response ka data ", response.data);
+      console.log("response ka data ", response.data.data);
 
       toast.success("data fetched successfully");
+
+      setSearchedResponseData(response.data.data);
+      setError("");
 
       // make a call to backend to seraching the user 
 
     } catch (error) {
 
-      console.log(error?.response?.data?.message);
+      console.log(error);
+
+      setError("not_found");
+      setSearchedResponseData(null);
 
       toast.error(error?.response?.data?.message);
 
@@ -78,6 +97,7 @@ function PopUps({ setshowPopUps, showPopUps,emailSearch ,setSearchedResponseData
     finally {
 
       setLoading(false);
+      setshowPopUps(false);
 
     }
   }
@@ -87,20 +107,23 @@ function PopUps({ setshowPopUps, showPopUps,emailSearch ,setSearchedResponseData
 
       {
 
-        loading && <Loader></Loader>
+        loading ? (<Loader></Loader>) : (
+
+          <div className="fixed inset-0  flex items-center justify-center z-50 backdrop-blur-sm bg-black/30">
+            <div className="bg-white m-8 p-8 rounded-xl shadow-xl max-w-md w-full">
+              <p className="text-base font-semibold mb-4 text-center">For Checking the status of Candidate,You need to burn one credit</p>
+
+              <div className='flex gap-5  justify-center items-centertext-xs font-semibold'>
+                <button onClick={() => setshowPopUps(false)} className=' px-10 border-[1px] border-gray-500 text-black  py-1 rounded-3xl'>Cancel</button>
+                <button onClick={checkStatusHandler} className=' py-1 px-10 border-[1px] border-gray-500  text-white rounded-3xl  bg-[#56b54a]'>Proceed</button>
+              </div>
+            </div>
+          </div>
+
+        )
 
       }
 
-      <div className="fixed inset-0  flex items-center justify-center z-50 backdrop-blur-sm bg-black/30">
-        <div className="bg-white m-8 p-8 rounded-xl shadow-xl max-w-md w-full">
-          <p className="text-base font-semibold mb-4 text-center">For Checking the status of Candidate,You need to burn one credit</p>
-
-          <div className='flex gap-5  justify-center items-centertext-xs font-semibold'>
-            <button onClick={() => setshowPopUps(false)} className=' px-10 border-[1px] border-gray-500 text-black  py-1 rounded-3xl'>Cancel</button>
-            <button onClick={checkStatusHandler} className=' py-1 px-10 border-[1px] border-gray-500  text-white rounded-3xl  bg-[#56b54a]'>Proceed</button>
-          </div>
-        </div>
-      </div>
 
     </div>
   )

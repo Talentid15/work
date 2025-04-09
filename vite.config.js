c9896b3815b9a928998f34bdea63130b1e3c1314
chavarahul <chavarahul7@gmail.com>
2025-04-09 18:12:59 +0530

    updated vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://talentid-backend-v2.vercel.app',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  }
+  base: '/', 
+  build: {
+    outDir: 'dist',
+  },
 });
-
-
